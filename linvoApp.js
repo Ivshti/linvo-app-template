ddc70f4b580b4511dbaea879c313e08e2945beae
Ivo Georgiev <ivo@linvo.org>
2014-01-12 19:25:52 +0200

    Include the LinvoDB into the project

--- linvoApp.js
+++ linvoApp.js
@@ -1,3 +1,6 @@
+var gui = require("nw.gui"); // in order to get the dataPath 
+var LinvoDB = require("linvodb")(gui.App.dataPath);
+
 function linvoAppCtrl($scope)
 {
     $scope.message = "main controller speaking";
