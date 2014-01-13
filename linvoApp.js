bce37f9c9b529eb48299f281f8772174a9dcb1c1
Ivo Georgiev <ivo@linvo.org>
2014-01-14 01:58:38 +0200

    Several modifications to make an example use of the Linvo DB

--- linvoApp.js
+++ linvoApp.js
@@ -1,6 +1,26 @@
 var gui = require("nw.gui"); // in order to get the dataPath 
 var LinvoDB = require("linvodb")(gui.App.dataPath);
 
+/* Ideally this should be a separate file for the model
+ */
+var User = new LinvoDB.Model("contact", {
+    name: String,
+    lastName: String,
+    phone: String,
+    birth: Date,
+    tags: [String]
+});
+//User.virtual("age")
+//User.virtual("fullName")
+
+var ivo = new User({
+    name: "Ivo",
+    lastName: "Georgiev"
+});
+ivo.save();
+/* 
+ */
+
 function linvoAppCtrl($scope)
 {
     $scope.message = "main controller speaking";
