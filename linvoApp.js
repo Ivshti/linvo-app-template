d55d9c1d7695a27fba486fd8b9f3fbf6a00b4bd4
Ivo Georgiev <ivo@linvo.org>
2014-01-17 01:56:37 +0200

    Selected contact to be highlighted

--- linvoApp.js
+++ linvoApp.js
@@ -7,8 +7,8 @@ var app = angular.module("contacts", []);
 app.controller("linvoAppCtrl", ["$scope", "contact", function($scope, contact)
 {
     $scope.contacts = contact.live({ });
-    $scope.selected = { };
     
+    $scope.selected = { };
     $scope.selected.contact = new contact(); // Selected contact    
     
     $scope.new = function() { $scope.selected.contact = new contact() };
