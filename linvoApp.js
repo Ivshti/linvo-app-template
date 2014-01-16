85ff26234756564ed7dd297bae986bb32ca2b1df
Ivo Georgiev <ivo@linvo.org>
2014-01-16 16:17:04 +0200

    Defined the Contact model as an Angular service

--- linvoApp.js
+++ linvoApp.js
@@ -1,27 +1,38 @@
 var gui = require("nw.gui"); // in order to get the dataPath 
 var LinvoDB = require("linvodb")(gui.App.dataPath);
 
+var app = angular.module("contacts", []);
+
 /* Ideally this should be a separate file for the model
  */
-var User = new LinvoDB.Model("contact", {
+var Contact = new LinvoDB.Model("contact", {
     name: String,
     lastName: String,
     phone: String,
     birth: Date,
-    tags: [String]
+    //tags: [String]
 });
 //User.virtual("age")
 //User.virtual("fullName")
 
-var ivo = new User({
-    name: "Ivo",
-    lastName: "Georgiev"
-});
-ivo.save(function(err, ids) { console.log(ids) });
+/*
+setTimeout(function() {
+    var ivo = new User({
+        name: "Ivo",
+        lastName: "Georgiev"
+    });
+    ivo.save(function(err, doc) { console.log(doc) });
+}, 5*1000);
+
+User.on("updated", function() { console.log("collection updated") });
+User.find({ name: "Ivo" }, function(err, res){console.log(res)});
+*/
+LinvoDB.createService(app, Contact);
 /* 
  */
 
-function linvoAppCtrl($scope)
+app.controller("linvoAppCtrl", ["$scope", "contact", function($scope, contact)
 {
     $scope.message = "main controller speaking";
-}
+    
+}]);
