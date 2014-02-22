0a18309aad0167da9cd416a98f49b7706a8c1460
Ivo Georgiev <ivo@linvo.org>
2014-02-22 22:36:53 +0200

    Expose contacts through the IPC ; currently not working because can't get node-webkit to work with the 'weak' module

--- linvoApp.js
+++ linvoApp.js
@@ -1,5 +1,6 @@
 var gui = require("nw.gui"); // in order to get the dataPath 
 var LinvoDB = require("linvodb");
+var LinvoIPC = require("linvoipc");
 var LinvoAPI = require("linvo-api4-client");
 
 LinvoDB.init(gui.App.dataPath);
@@ -10,6 +11,7 @@ var app = angular.module("contacts", []);
 app.controller("linvoAppCtrl", ["$scope", "contact", function($scope, contact)
 {
     contact.setupSync(linvoAPI);
+    LinvoIPC.defineService("contacts", function() { return contact });
     $scope.contacts = contact.live({ });
     //$scope.contacts = contact.find({}).limit(5).sort({ _ctime: 1 }).live(); // we can do more advanced stuff
 
