28162cdc0391c7c5c79ed8e7f0d65e99bd4ebb8b
Ivo Georgiev <ivo@linvo.org>
2014-02-22 21:54:20 +0200

    An example of defered/asynchronous factory creation - we'll need that for the IPC but we'll need to patch angular for this

--- linvoApp.js
+++ linvoApp.js
@@ -7,8 +7,19 @@ var linvoAPI = new LinvoAPI();
 
 var app = angular.module("contacts", []);
 
-app.controller("linvoAppCtrl", ["$scope", "contact", function($scope, contact)
+app.factory("test",["$q",function($q)
 {
+    var def = $q.defer();
+    setTimeout(function()
+    {
+        def.resolve("test test");
+    }, 1000);
+    return def.promise;
+}]);
+
+app.controller("linvoAppCtrl", ["$scope", "contact", "test", function($scope, contact, test)
+{
+    console.log(test);
     contact.setupSync(linvoAPI);
     $scope.contacts = contact.live({ });
     //$scope.contacts = contact.find({}).limit(5).sort({ _ctime: 1 }).live(); // we can do more advanced stuff
